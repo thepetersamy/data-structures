0553e63b121815917ba973dd6511b2f3e96621a6
peter592 <petersamy592@gmail.com>
2021-04-06 20:56:19 +0200

    created repo

---
+++ CharArrayStack.h
@@ -0,0 +1,118 @@
+#ifndef REPO_CHARARRAYSTACK_H
+#define REPO_CHARARRAYSTACK_H
+
+
+struct Stack {
+    char* arr;
+    int sp;
+    int size;
+
+    Stack() {
+        sp = -1;
+        size = 0;
+        arr = NULL;
+    }
+};
+
+
+bool init(Stack* s, int size) {
+
+    s->arr = (char*)malloc(sizeof(char) * size);
+
+    if (s->arr == NULL)
+        return false;
+
+    s->size = size;
+
+    return true;
+}
+
+
+int push(Stack* s, char item) {
+
+    // check if initialised
+    if (s->arr == NULL) {
+        return -1;
+    }
+
+    // check for overflow
+    if (s->sp + 1 == s->size) {
+        return -2;
+    }
+
+    s->sp++;
+    s->arr[s->sp] = item;
+}
+
+
+int pop(Stack* s, char* item) {
+
+    // check if initialized
+    if (s->arr == NULL) {
+        return -1;
+    }
+
+    // check for underflow
+    if (s->sp == -1) {
+        return -2;
+    }
+
+    *item = s->arr[s->sp];
+    s->sp--;
+
+    return 1;
+}
+
+int peek(Stack s, char* item){
+
+    // check for init
+    if (s.arr == NULL)
+        return -1;
+
+    // check for underflow
+    if (s.sp == -1) {
+        return -2;
+    }
+
+    *item = s.arr[s.sp];
+
+    return 1;
+}
+
+
+
+bool isStackEmpty(Stack s) {
+    if (s.sp == -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int getCurrentStackSize(Stack s) {
+    return s.size;
+}
+
+bool stackContains(Stack s, char item) {
+    for (int i = 0; i < s.sp; i++) {
+        if (s.arr[i] == item) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int getFrequencyOf(Stack s, int item) {
+
+    int frequencyCounter = 0;
+    for (int i = 0; i < s.sp; i++) {
+        if (s.arr[i] == item) {
+            frequencyCounter++;
+        }
+    }
+    return frequencyCounter;
+}
+
+
+#endif //REPO_CHARARRAYSTACK_H
---
+++ StackApplications.h
@@ -0,0 +1,147 @@
+#ifndef REPO_STACKAPPLICATIONS_H
+#define REPO_STACKAPPLICATIONS_H
+
+
+bool isNumber(char c){
+    return c >= '0' && c <= '9';
+}
+
+int priority(char op) {
+
+    if (op == '*' || op == '/') {
+        return 3;
+    }
+    else if (op == '+' || op == '-') {
+        return 2;
+    }
+    else if (op == '^') {
+        return 4;
+    }
+    return 10;
+}
+
+void infixToPostfix(char infix[], char result[]) {
+
+    Stack s;
+    char postfix[100];
+
+    init(&s, strlen(infix));
+
+
+    int counter = 0;
+    for (int i = 0; i < strlen(infix); i++) {
+
+        if (isNumber(infix[i])) {
+            postfix[counter++] = infix[i];
+            //            printf("%d", infix[i]);
+        }
+        else if (infix[i] == '(') {
+            push(&s, infix[i]);
+        }
+        else if (infix[i] == ')') {
+            char top;
+            peek(s, &top);
+            while (top != '(') {
+                pop(&s, &top);
+                postfix[counter++] = top;
+                peek(s, &top);
+            }
+            pop(&s, &top); //dispose the )
+        }
+
+        else {
+            char op;
+            if (peek(s, &op) >= 0)
+            {
+                while (priority(op) >= priority(infix[i])) {
+
+                    char popped;
+                    pop(&s, &popped);
+                    postfix[counter++] = popped;
+
+                    peek(s, &op);
+                }
+            }
+            push(&s, infix[i]); //dont forget to push the actual operator
+
+        }
+        postfix[counter + 1] = '\0';
+    }
+    char item;
+
+    while (s.sp != -1) {
+        pop(&s, &item);
+        postfix[counter++] = item;
+        postfix[counter + 1] = '\0';
+
+    }
+
+    strcpy(result, postfix);
+
+
+}
+
+
+
+bool isMatching(char c1, char c2){
+    return  (c1 == '(' && c2 == ')') ||
+            (c1 == '{' && c2 == '}') ||
+            (c1 == '[' && c2 == ']') ||
+            (c1 == '<' && c2 == '>');
+}
+
+
+bool isBalanced(const char* exp){
+
+    Stack s;
+
+    init(&s, strlen(exp));
+
+    for(int i=0; i<strlen(exp); i++){
+
+        // if opening bracket found push to stack
+        if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{' || exp[i] == '<'){
+            push(&s, exp[i]);
+        }
+
+
+        // if closing bracket found pop
+        if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}' || exp[i] == '>'){
+
+            // if nothing to pop then not balanced
+            if(isStackEmpty(s)){
+                return false;
+            }
+
+            char popped;
+            pop(&s, &popped);
+
+            if(!isMatching(popped, exp[i])){
+                return false;
+            }
+
+
+        }
+    }
+
+    return s.sp == -1;
+
+}
+
+
+void reverseStingByStack(char* str){
+
+    Stack s;
+    init(&s, strlen(str));
+
+    for(int i=0; i<strlen(str); i++){
+        push(&s, str[i]);
+    }
+
+    int counter = 0;
+    while(!isStackEmpty(s)){
+        pop(&s, &str[counter++]);
+    }
+}
+
+#endif //REPO_STACKAPPLICATIONS_H
